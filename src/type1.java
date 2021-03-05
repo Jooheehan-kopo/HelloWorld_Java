3b0a53e210a696660a43d9f55748dfa949d64d65
Jooheehan-kopo <79953962+Jooheehan-kopo@users.noreply.github.com>
2021-03-05 15:49:13 +0900

    Update type1.java

--- src/type1.java
+++ src/type1.java
@@ -1,11 +1,11 @@
 
-public class type1 {
+public class type2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi, there");
 		System.out.println("hi!");
-		System.out/println("nooooo");
+		System.out.println("yes");
 
 	}
 
